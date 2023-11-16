d5f4fdfe367ae47261b9ee3ac147bf7ba6d51316
JonathanMarley <jonathanmarley2536@gmail.com>
2023-11-15 21:51:56 -0600

    Finalzada

---
+++ app/src/main/java/com/example/ejercicio2_4_jonathanmarley/SQLiteConexion.java
@@ -0,0 +1,25 @@
+package com.example.ejercicio2_4_jonathanmarley;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.ejercicio2_4_jonathanmarley.Transacciones.Transacciones;
+
+public class SQLiteConexion extends SQLiteOpenHelper  {
+
+    public SQLiteConexion(Context context, String bddname, SQLiteDatabase.CursorFactory factory, int version){
+        super(context, bddname, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(Transacciones.CreateTableFirmas);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(Transacciones.DropTableFirmas);
+        onCreate(db);
+    }
+}
